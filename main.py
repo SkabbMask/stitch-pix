38590b625fc7bbe5625e86104e495e8201905cb3
Fabian <fabian.rosdalen@gmail.com>
2024-07-29 01:04:56 +0200

    comprehend more

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 import argparse
 
 symbols_dimension = 10
@@ -21,7 +21,7 @@ def image_to_2d_array(image_path):
     for y in range(height):
         for x in range(width):
             r, g, b = img.getpixel((x, y))
-            arr[y][x] = r
+            arr[y][x] = f"{r:03}{g:03}{b:03}"
 
     return arr
 
@@ -31,41 +31,91 @@ def create_empty_image_to_size(arr):
     image = Image.new("RGB", (width, height))
     return image
 
-def fill_pattern(symbols_image, empty_image, pixels):
+def fill_pattern(symbols_array, empty_image, pixels, pixel_dictionary):
     for i, row in enumerate(pixels):
-        for j, symbol_value in enumerate(row):
-            if symbol_value == 255:
-                symbol_value = 6
-
-            symbol_x = (symbol_value % symbols_dimension) * symbols_dimension
-            symbol_y = (symbol_value // symbols_dimension) * symbols_dimension
-            
-            symbol = symbols_image.crop((symbol_x, symbol_y, symbol_x + symbols_dimension, symbol_y + symbols_dimension))
-            
+        for j, pixel_value in enumerate(row):
             output_x = j * symbols_dimension
             output_y = i * symbols_dimension
-            empty_image.paste(symbol, (output_x, output_y))
+            empty_image.paste(symbols_array[pixel_dictionary[pixel_value]], (output_x, output_y))
     return empty_image
 
-def generate_cross_stitch_pattern(input_image_path, symbol_image_path, output_image_path):
-    symbol_image = Image.open(symbol_image_path)
+def get_unique_pixels(pixel_values):
+    unique_pixels = []
+    for row in pixel_values:
+        for column in row:
+            if column in unique_pixels:
+                continue
+            unique_pixels.append(column)
+    return unique_pixels
+
+def make_pixel_dictionary(unique_pixels):
+    pixel_dictionary = {}
+    for i in range(len(unique_pixels)):
+        pixel_dictionary[unique_pixels[i]] = i
+    return pixel_dictionary
+
+def make_symbol_array(symbols_image):
+    symbols_array = []
+    width, height = symbols_image.size
+    for x in range(int(width / symbols_dimension)):
+        for y in range(int(height / symbols_dimension)):
+            symbol_x = x * symbols_dimension
+            symbol_y = y * symbols_dimension
+            symbol = symbols_image.crop((symbol_x, symbol_y, symbol_x + symbols_dimension, symbol_y + symbols_dimension))
+            symbols_array.append(symbol)
+    return symbols_array
+
+def create_symbol_color_reference(unique_pixels, pixel_dictionary, symbols_array):
+    image = Image.new("RGB", (symbols_dimension*10, (symbols_dimension*len(unique_pixels)) + 10), "white")
+    draw = ImageDraw.Draw(image)
+    for i in range(len(unique_pixels)):
+        row_y = 5 + (i * symbols_dimension)
+
+        index = pixel_dictionary[unique_pixels[i]]
+        symbol = symbols_array[index]
+        image.paste(symbol, (5, row_y))
+
+        rgb_string = unique_pixels[i]
+        r = int(rgb_string[:3])
+        g = int(rgb_string[3:6])
+        b = int(rgb_string[6:])
+        draw.rectangle([10 + symbols_dimension, row_y, 10 + (symbols_dimension*2), row_y + symbols_dimension], fill=(r,g,b))
+    return image
+
+def generate_cross_stitch_pattern(input_image_path, symbols_image_path, output_image_path):
+    symbols_image = Image.open(symbols_image_path)
+    symbols_array = make_symbol_array(symbols_image)
 
     pixel_values = image_to_2d_array(input_image_path)
+
+    unique_pixels = get_unique_pixels(pixel_values)
+
+    if len(symbols_array) < len(unique_pixels):
+        print("Not enough symbols (" + str(len(symbols_array)) + ") for unique pixels (" + str(len(unique_pixels)) + ")")
+        return
+
+    pixel_dictionary = make_pixel_dictionary(unique_pixels)
+    color_reference = create_symbol_color_reference(unique_pixels, pixel_dictionary, symbols_array)
+    color_reference_path = output_image_path+"\color_reference.png"
+    color_reference.save(color_reference_path)
+    print("Saved color reference to " + color_reference_path)
+
     empty_image = create_empty_image_to_size(pixel_values)
-    pattern_image = fill_pattern(symbol_image, empty_image, pixel_values)
+    pattern_image = fill_pattern(symbols_array, empty_image, pixel_values, pixel_dictionary)
     
-    pattern_image.save(output_image_path)
-    print("Saved image to " + output_image_path)
+    pattern_path = output_image_path+"\pattern.png"
+    pattern_image.save(pattern_path)
+    print("Saved image to " + pattern_path)
 
 def main():
     parser = argparse.ArgumentParser(description="Generate cross-stitch pattern from image.")
     parser.add_argument("input_image_path", help="Path to the input image.")
-    parser.add_argument("symbol_image_path", help="Path to the symbol image.")
-    parser.add_argument("output_image_path", help="Path to save the generated pattern image.")
+    parser.add_argument("symbols_image_path", help="Path to the symbol image.")
+    parser.add_argument("output_image_path", help="Path to map to save generated pattern and color reference.")
 
     args = parser.parse_args()
 
-    generate_cross_stitch_pattern(args.input_image_path, args.symbol_image_path, args.output_image_path)
+    generate_cross_stitch_pattern(args.input_image_path, args.symbols_image_path, args.output_image_path)
 
 if __name__ == "__main__":
     main()
